7e0722e024a3badb5c9f3e9e8a8e53f30f43f7aa
Марсель Мубаракшин <marselmubaraksin231@mail.ru>
2023-11-20 19:23:26 +0300

    Версия 2

--- 20.11.2023/Program.cs
+++ 20.11.2023/Program.cs
@@ -1,85 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Calculator
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Введите выражение:");
+        string input = Console.ReadLine();
+
+        double result = CalculateExpression(input);
+        Console.WriteLine("Результат: " + result);
+    }
+
+    static double CalculateExpression(string input)
+    {
+        string[] tokens = input.Split(' ');
+
+        Stack<double> numbersStack = new Stack<double>();
+        Stack<char> operatorsStack = new Stack<char>();
+
+        Dictionary<char, Func<double, double, double>> operators = new Dictionary<char, Func<double, double, double>>();
+        operators['+'] = (a, b) => a + b;
+        operators['-'] = (a, b) => a - b;
+        operators['*'] = (a, b) => a * b;
+        operators['/'] = (a, b) => a / b;
+
+        foreach (string token in tokens)
         {
-            Console.WriteLine("Введите выражение:");
-            string input = Console.ReadLine();
-
-            int result = CalculateExpression(input);
-            Console.WriteLine("Результат: " + result);
-        }
-
-        static int CalculateExpression(string input)
-        {
-            string[] tokens = input.Split(' ');
-
-            Stack<int> numbersStack = new Stack<int>();
-            Stack<char> operatorsStack = new Stack<char>();
-
-            foreach (string token in tokens)
+            if (double.TryParse(token, out double number))
             {
-                if (int.TryParse(token, out int number))
+                numbersStack.Push(number);
+            }
+            else
+            {
+                char op = token[0];
+
+                while (operatorsStack.Count > 0 && GetPrecedence(op) <= GetPrecedence(operatorsStack.Peek()))
                 {
-                    numbersStack.Push(number);
+                    double operand2 = numbersStack.Pop();
+                    double operand1 = numbersStack.Pop();
+                    double result = operators[operatorsStack.Pop()](operand1, operand2);
+                    numbersStack.Push(result);
                 }
-                else
-                {
-                    char op = token[0];
 
-                    while (operatorsStack.Count > 0 && GetPrecedence(op) <= GetPrecedence(operatorsStack.Peek()))
-                    {
-                        int result = ApplyOperator(numbersStack.Pop(), numbersStack.Pop(), operatorsStack.Pop());
-                        numbersStack.Push(result);
-                    }
-
-                    operatorsStack.Push(op);
-                }
+                operatorsStack.Push(op);
             }
-
-            while (operatorsStack.Count > 0)
-            {
-                int result = ApplyOperator(numbersStack.Pop(), numbersStack.Pop(), operatorsStack.Pop());
-                numbersStack.Push(result);
-            }
-
-            return numbersStack.Pop();
         }
 
-        static int GetPrecedence(char op)
+        while (operatorsStack.Count > 0)
         {
-            switch (op)
-            {
-                case '+':
-                case '-':
-                    return 1;
-                case '*':
-                case '/':
-                    return 2;
-                default:
-                    return 0;
-            }
+            double operand2 = numbersStack.Pop();
+            double operand1 = numbersStack.Pop();
+            double result = operators[operatorsStack.Pop()](operand1, operand2);
+            numbersStack.Push(result);
         }
 
-        static int ApplyOperator(int b, int a, char op)
-        {
-            switch (op)
-            {
-                case '+':
-                    return a + b;
-                case '-':
-                    return a - b;
-                case '*':
-                    return a * b;
-                case '/':
-                    return a / b;
-                default:
-                    throw new ArgumentException("Неверная операция");
-            }
-        }
+        return numbersStack.Pop();
+    }
+
+    static int GetPrecedence(char op)
+    {
+        string operators = "+ - * /";
+        return operators.IndexOf(op) / 2;
     }
 }
